c9820f66777c15579c8fec6ddd0d81ee5d0870e8
lucenac <joaopedrolucena568@gmail.com>
2024-03-15 11:59:06 -0300

    first commit

---
+++ src/main/java/br/com/projeto/utd/projetolivraria/ProjetoLivrariaApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.utd.projetolivraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLivrariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLivrariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto/utd/projetolivraria/ProjetoLivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.utd.projetolivraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLivrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
